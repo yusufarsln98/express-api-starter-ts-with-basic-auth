21d064490532e81c63a362255fb49f01072c60dd
Yusuf Arslan <yusufarsln98@outlook.com>
2024-03-17 19:57:48 +0300

    feat: helper functions for hashing password and tokens

---
+++ src/utils/generateJwt.ts
@@ -0,0 +1,11 @@
+// utils/generateJwt.ts
+import jwt from 'jsonwebtoken';
+import User from '../interfaces/User';
+require('dotenv').config();
+
+const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
+
+export const generateJwt = (user: User) => {
+  const payload = { username: user.username };
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: '30m' }); // Set expiration to 30 minutes
+};

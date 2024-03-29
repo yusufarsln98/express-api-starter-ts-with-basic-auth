a6f078fc00924a30ddcd03eab7eee475728689a0
Yusuf Arslan <yusufarsln98@outlook.com>
2024-03-29 21:45:10 +0300

    feat: role encoding added to jwt generator

--- src/utils/generateJwt.ts
+++ src/utils/generateJwt.ts
@@ -6,6 +6,6 @@ require('dotenv').config();
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
 export const generateJwt = (user: User) => {
-  const payload = { username: user.username };
+  const payload = { username: user.username, role: user.role };
   return jwt.sign(payload, JWT_SECRET, { expiresIn: '30m' }); // Set expiration to 30 minutes
 };

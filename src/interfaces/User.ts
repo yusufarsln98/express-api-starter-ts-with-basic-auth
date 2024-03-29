1185d2b839990e91408f4a36d32ea9f700323121
Yusuf Arslan <yusufarsln98@outlook.com>
2024-03-29 21:46:52 +0300

    chore: Role union added to user type

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,15 +1,17 @@
 import MessageResponse from './MessageResponse';
 
+type Role = 'admin' | 'user';
 export default interface User {
   id: number;
   username: string;
   password: string;
   name: string;
   token?: string | null;
+  role: Role;
 }
 
 interface LoginResponse extends MessageResponse {
   user: Pick<User, 'id' | 'token'>;
 }
 
-export { LoginResponse };
+export { LoginResponse, Role };
